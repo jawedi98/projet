3d8ab947bf389d7debab3d815680458b8474e49d
jawedi98 <azizjawedi712@gmail.com>
2019-04-29 12:18:08 +0200

    new commit

--- views/supprimercarte.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include "../core/carteC.php";
-$carteC=new carteC();
-if (isset($_POST["id"])){
-	$carteC->supprimerCarte($_POST["id"]);
-echo "<META http-equiv='refresh' content='0;URL=editcarte.php'>";
-}
-
-?>
--- views/supprimerclient.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include "../core/clientC.php";
-$clientC=new clientC();
-if (isset($_POST["id"])){
-	$clientC->supprimerClient($_POST["id"]);
-echo "<META http-equiv='refresh' content='0;URL=editable.php'>";
-}
-
-?>
